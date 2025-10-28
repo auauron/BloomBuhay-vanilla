c3670990440135df6027704960e885943bbc5db9
Sophia Marielle C. Mendoza <sophiamarielle.mendoza-24@cpu.edu.ph>
2025-10-29 03:31:36 +0800

    chore: add route path for signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
