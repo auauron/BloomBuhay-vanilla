d640140793c66bafd8dbb37b2f732f2b23116a6c
Sophia Marielle C. Mendoza <sophiamarielle.mendoza-24@cpu.edu.ph>
2025-10-29 03:00:00 +0800

    feat: added dashboard frontend (static) including uniform header and sidebar with icons from lucide

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+        colors: {
+              bloomWhite: '#FFF6F6',
+              bloomPink: '#F875AA',
+              bloomYellow: '#F3E198',
+              bloomBlack: '#474747'
+      },
       fontFamily: {
         rubik: ['Rubik', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
@@ -13,4 +19,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
