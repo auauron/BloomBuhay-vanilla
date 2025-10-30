73787415df4c9e1ceab09096069439e842f3eab3
saessei <danielleanne.poral-22@cpu.edu.ph>
2025-10-30 22:51:41 +0800

    chore: make next button into a separate component

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
 import "../index.css";
-import Header from "../components/Header";
+import Header from "../components/ui/Header";
 import Sidebar from "../components/Sidebar";
 import InputField from "../components/inputField";
 import { Edit, LogOut } from "lucide-react";
