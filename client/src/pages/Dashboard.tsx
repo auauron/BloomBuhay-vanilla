48ab316faf209d1a3b0a4e31b96b77765e87befb
auauron <ahronjohn.alera-18@cpu.edu.ph>
2025-10-30 23:05:46 +0800

    Reapply "chore: make next button into a separate component"
    This reverts commit 7de2eecc4b3c85fcacdbb464312882ae8391300e.

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "../index.css";
-import Header from "../components/Header";
+import Header from "../components/ui/Header";
 import Sidebar from "../components/Sidebar";
 
 export default function Dashboard() {
