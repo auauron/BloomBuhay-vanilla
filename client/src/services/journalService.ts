2e6bcff7cbdff2e44152fe9e1c98f07c2875bf94
auauron <ahronjohn.alera-18@cpu.edu.ph>
2025-11-08 04:28:29 +0800

    feat: start journal service

---
+++ client/src/services/journalService.ts
@@ -0,0 +1,7 @@
+import { authService } from "./authService";
+import { Note, Album, Photo } from "../components/journal/types";
+
+const API_URL = "http://localhost:3000/api/journal";
+
+// TYPE DEFINITIONS
+
